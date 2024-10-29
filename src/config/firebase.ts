605d31f4661a3464f0da051a6eade72325ddb933
Mariano <marianolumbreras@gmail.com>
2024-10-28 23:26:50 -0300

    GP a Render 1.1

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,17 +1,20 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import { getAnalytics } from 'firebase/analytics';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBj8PFJOkpu6hS_5GTWnqGOi_lyy4El_2I",
+  apiKey: "AIzaSyAYqFK3PhdSb6rJw2hbHRkbqxN0Z_PYOyU",
   authDomain: "expense-tracker-bolt.firebaseapp.com",
   projectId: "expense-tracker-bolt",
   storageBucket: "expense-tracker-bolt.appspot.com",
-  messagingSenderId: "123456789012",
-  appId: "1:123456789012:web:abc123def456ghi789jkl"
+  messagingSenderId: "475275015176",
+  appId: "1:475275015176:web:fb91af6b64bccc0f1d0dbd",
+  measurementId: "G-LVP209YV1D" // Opcional
 };
 
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 const db = getFirestore(app);
 const auth = getAuth(app);
 auth.useDeviceLanguage();
