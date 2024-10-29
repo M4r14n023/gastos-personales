7836f3e3bfdf82a1eea3dba0f68c67ce3a1af130
Mariano <marianolumbreras@gmail.com>
2024-10-29 18:42:00 -0300

    GP a Render 2.0

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
-    sourcemap: true
-  }
+    sourcemap: true,
+  },
+  server: {
+    host: '0.0.0.0', // Permitir que escuche en todas las interfaces
+    port: Number(process.env.PORT) || 4173, // Usa el puerto proporcionado por Render o 4173 como predeterminado
+  },
 });
