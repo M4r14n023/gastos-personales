d7fe132aabada3769c723a554a23f02d9e86d241
Mariano <marianolumbreras@gmail.com>
2024-11-08 20:28:12 -0300

    Tutorial 1.1

--- src/components/Presupuesto.tsx
+++ src/components/Presupuesto.tsx
@@ -329,7 +329,7 @@ export const Presupuesto: React.FC = () => {
 
 
                      {/* Botón de Abrir Tutorial */}
-    <button onClick={() => window.open('./tutorial-presupuesto.html', '_blank')} className="mb-4 px-4 py-2 text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 rounded-md">
+    <button onClick={() => window.open('/tutorial-presupuesto.html', '_blank')} className="mb-4 px-4 py-2 text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 rounded-md">
       ?
     </button>
 
